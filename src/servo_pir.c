9401e98ff161e4bcedf6f70ca5e7906c39d51179
tensingn <tensingn@oregonstate.edu>
2024-01-03 20:57:40 -0500

    removing comments

--- src/servo_pir.c
+++ src/servo_pir.c
@@ -10,18 +10,6 @@ const uint PIR_PIN = 0;
 
 bool motion_detected = false;
 
-// void pir_irq(uint pin, uint32_t event)
-// {
-//     if (event == GPIO_IRQ_EDGE_RISE)
-//     {
-//         motion_detected = true;
-//     }
-//     else
-//     {
-//         motion_detected = false;
-//     }
-// }
-
 int main()
 {
     stdio_init_all();
@@ -31,8 +19,6 @@ int main()
     setup_pin(LED_PIN, GPIO_OUT);
 
     setup_pin(PIR_PIN, GPIO_IN);
-
-    // gpio_set_irq_enabled_with_callback(PIR_PIN, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &pir_irq);
 
     while (true)
     {
@@ -48,18 +34,6 @@ int main()
             set_servo_position(SERVO_PIN, 400);
         }
         sleep_ms(250);
-        // if (motion_detected)
-        // {
-        //     gpio_put(LED_PIN, 1);
-        //     set_servo_position(SERVO_PIN, 2400);
-        //     sleep_ms(1000);
-        // }
-        // else
-        // {
-        //     gpio_put(LED_PIN, 0);
-        //     set_servo_position(SERVO_PIN, 400);
-        //     sleep_ms(1000);
-        // }
     }
 
     return 0;
